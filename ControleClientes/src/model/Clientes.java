4e3c84c8b53f4b6dca73265434fd54000c7d5abd
Barbara <barbaraalessandramaas@gmail.com>
2021-05-04 13:55:07 -0300

    Arquivos

---
+++ ControleClientes/src/model/Clientes.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Lucas
+ */
+public class Clientes {
+
+    private String razaoSocial;
+    private String cnpj;
+    private String nome_fantasia;
+    private String telefone;
+    private String email;
+    private String cidade;
+    private String endereco;
+    private String bairro;
+    private LocalDate vencimento;
+    private boolean cliente_ativo;
+
+    public String getRazaoSocial() {
+        return razaoSocial;
+    }
+
+    public void setRazaoSocial(String razaoSocial) {
+        this.razaoSocial = razaoSocial;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getNome_fantasia() {
+        return nome_fantasia;
+    }
+
+    public void setNome_fantasia(String nome_fantasia) {
+        this.nome_fantasia = nome_fantasia;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public LocalDate getVencimento() {
+        return vencimento;
+    }
+
+    public void setVencimento(LocalDate vencimento) {
+        this.vencimento = vencimento;
+    }
+
+    public boolean isCliente_ativo() {
+        return cliente_ativo;
+    }
+
+    public void setCliente_ativo(boolean cliente_ativo) {
+        this.cliente_ativo = cliente_ativo;
+    }
+}
